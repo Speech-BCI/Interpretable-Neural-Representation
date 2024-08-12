dd328351077c8a74f3a470a35ea7947cae69328c
Speech-BCI <ai.brainmap@gmail.com>
2024-08-12 12:24:29 +0900

    Update class_balanced_loss.py

--- modules/class_balanced_loss.py
+++ modules/class_balanced_loss.py
@@ -15,13 +15,3 @@ def apply_class_balanced_loss(cl_loss, labels, num_class_trials, beta):
 
     weighted_loss = cl_loss.squeeze() * weights
     return weighted_loss.mean()
-
-def scaling_neg_dist(neg_dist, scale_factor = 0.7):
-    batch_size, num_samples = neg_dist.shape
-    sorted_indices = torch.argsort(neg_dist, dim=1, descending=False)  # Sort in descending order
-    ranks = torch.argsort(sorted_indices, dim=1).float()
-    x = np.linspace(0, math.e, num_samples)
-    y = (1.0 - scale_factor) * np.exp(-x) + scale_factor
-    y_scaler = torch.from_numpy(y).to(neg_dist.device)
-    scaled_neg_dist = neg_dist * y_scaler[ranks.long()]
-    return scaled_neg_dist
